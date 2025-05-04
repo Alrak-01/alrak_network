693c57584a61453ba5eead90d575910c26a80233
Alrak-01 <lekanraheem827@gmail.com>
2025-05-05 00:18:07 +0100

    Route and Views

---
+++ resources/views/developers/index.blade.php
@@ -0,0 +1,14 @@
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Alrak Network | Home</title>
+</head>
+<body>
+    <h2>We're currently in the developers page</h2>
+    <p>View the list of developers below</p>
+    <ul>
+    <li>Alracode</li>
+    </ul>
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Alrak Network</title>
+</head>
+<body>
+    <h1>Hello world!!!</h1>
+    <p>click the button to get started</p>
+    <a href="/developers">
+        Get started...
+    </a>
+</body>
+</html>
