49807eef7c2ace63ff85e1532b4a891538ec9c63
Alrak-01 <lekanraheem827@gmail.com>
2025-05-20 00:42:07 +0100

    using factory to populate date for testing

--- app/Models/developer.php
+++ app/Models/developer.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class developer extends Model
+class Developer extends Model
 {
     protected $fillable = ["name", "skill", "bio"];
     /** @use HasFactory<\Database\Factories\DeveloperFactory> */
